286067eaca4720bef488e37b2d6a4068b4e00adb
a21n <auronmatrix@users.noreply.github.com>
2019-08-13 19:14:01 +0200

    fix routing path for GH_PAGES

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ const routerBase =
   process.env.DEPLOY_ENV === "GH_PAGES"
     ? {
         router: {
-          base: "/<repository-name>/"
+          base: "/seedhodler/"
         }
       }
     : {};
